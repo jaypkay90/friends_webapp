22b92da299f0e1d0b32d0cdc4bb4ebd646db9608
JP Klöcker <jpkloecker@web.de>
2024-05-07 09:54:35 +0200

    Enable authentication with env vars

---
+++ config.py
@@ -0,0 +1,7 @@
+# Configure session to use filesystem (instead of signed cookies)
+SESSION_PERMANENT = False
+SESSION_TYPE = "filesystem"
+
+# Username and password to access page
+SITE_USER = "user"
+SITE_PASSWORD = "pass"
